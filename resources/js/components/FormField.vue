0efc94da1897f3a730cac77023080834d7f5e297
RVxLab <richard@rvx.works>
2020-08-27 14:41:50 +0200

    Added a debounce to ensure multiple calls don't cause the select to become blank

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,67 +1,79 @@
 <script>
-    export default {
-        data: () => ({
-            dependsOnValue: null,
-        }),
+export default {
+    data: () => ({
+        dependsOnValue: null,
+        watcherDebounce: null,
+        watcherDebounceTimeout: 100,
+    }),
 
-        created() {
-            if(this.field.dependsOn !== undefined) {
-                this.registerDependencyWatchers(this.$root)
-            }
-        },
+    created() {
+        if (this.field.dependsOn !== undefined) {
+            this.registerDependencyWatchers(this.$root)
+        }
+    },
 
-        methods: {
-            registerDependencyWatchers(root) {
-                root.$children.forEach(component => {
-                    if (this.componentIsDependency(component)) {
-                        if( component.selectedResourceId !== undefined ) {
-                            // BelongsTo field
-                            component.$watch('selectedResourceId', this.dependencyWatcher, {immediate: true})
-                            this.dependencyWatcher(component.selectedResourceId)
-                        }
-                        else if( component.value !== undefined ) {
-                            // Text based fields
-                            component.$watch('value', this.dependencyWatcher, {immediate: true})
-                            this.dependencyWatcher(component.value)
-                        }
+    beforeDestroy() {
+        if (this.watcherDebounce) {
+            clearTimeout(this.watcherDebounce)
+        }
+    },
+
+    methods: {
+        registerDependencyWatchers(root) {
+            root.$children.forEach(component => {
+                if (this.componentIsDependency(component)) {
+                    if (component.selectedResourceId !== undefined) {
+                        // BelongsTo field
+                        component.$watch('selectedResourceId', this.dependencyWatcher, {immediate: true})
+                        this.dependencyWatcher(component.selectedResourceId)
+                    } else if (component.value !== undefined) {
+                        // Text based fields
+                        component.$watch('value', this.dependencyWatcher, {immediate: true})
+                        this.dependencyWatcher(component.value)
                     }
-
-                    this.registerDependencyWatchers(component)
-                })
-            },
-            componentIsDependency(component) {
-                if(component.field === undefined) {
-                    return false
                 }
 
-                return component.field.attribute === this.field.dependsOn
-            },
-            dependencyWatcher(value) {
-                if(value === this.dependsOnValue) {
+                this.registerDependencyWatchers(component)
+            })
+        },
+        componentIsDependency(component) {
+            if (component.field === undefined) {
+                return false
+            }
+
+            return component.field.attribute === this.field.dependsOn
+        },
+        dependencyWatcher(value) {
+            clearTimeout(this.watcherDebounce)
+            this.watcherDebounce = setTimeout(() => {
+                if (value === this.dependsOnValue) {
                     return
                 }
 
                 this.dependsOnValue = value
 
                 this.clearSelection()
-				setTimeout(() => {
-	                this.initializeComponent()
-				}, 1000);
-            },
-        },
+                setTimeout(() => {
+                    this.initializeComponent()
+                }, 1000)
 
-        computed: {
-            queryParams() {
-                return {
-                    params: {
-                        current: this.selectedResourceId,
-                        first: this.initializingWithExistingResource,
-                        search: this.search,
-                        withTrashed: this.withTrashed,
-                        dependsOnValue: this.dependsOnValue,
-                    },
-                }
-            },
+                this.watcherDebounce = null
+            }, this.watcherDebounceTimeout)
         },
-    }
+    },
+
+    computed: {
+        queryParams() {
+            return {
+                params: {
+                    current: this.selectedResourceId,
+                    first: this.initializingWithExistingResource,
+                    search: this.search,
+                    withTrashed: this.withTrashed,
+                    dependsOnValue: this.dependsOnValue,
+                },
+            }
+        },
+    },
+}
 </script>
